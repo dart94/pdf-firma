500b5d538447ad8417fa6477436787bc5a5cfbc4
dart94 <167586944+dart94@users.noreply.github.com>
2024-11-29 15:53:14 -0700

    pruebas models

--- app/models.py
+++ app/models.py
@@ -7,14 +7,10 @@ class SignatureRequest(db.Model):
     id = db.Column(db.String(36), primary_key=True)
     filename = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
-    expires_at = db.Column(db.DateTime, nullable=False, default=lambda: SignatureRequest.utcnow_with_timezone() + timedelta(days=7))
+    expires_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc) + timedelta(days=7))
     is_signed = db.Column(db.Boolean, default=False)
     signed_pdf = db.Column(db.LargeBinary, nullable=True)
 
-    @staticmethod
-    def utcnow_with_timezone():
-        return datetime.now(timezone.utc)
-    
     def is_expired(self):
         # Convertir expires_at a datetime con zona horaria UTC si no lo tiene
         if self.expires_at.tzinfo is None:
